68561a6d29c8c4e8102b023a2c89f5caa9f16848
broccoli88 <kode.kudaki@gmail.com>
2022-02-28 18:07:42 +0100

    stopwatch functionality

--- js/script.js
+++ js/script.js
@@ -1,47 +1,89 @@
-function stopwatch() {}
 
-let duration = 0;
-let startTime = null;
-let stopTime = null;
-let started = false;
+function stopwatch() {
 
-// START TIME
+  const startButton = document.querySelector(`#start`);
+  const stopButton = document.querySelector(`#stop`);
+  const resetButton = document.querySelector(`#reset`);
 
-function start() {
-  if (started === false) {
-    started = true;
-    startTime = Date.now();
-    return startTime;
-  } else {
-    return `Counting already started.`;
+  const digit1 = document.querySelector(`#digit1`)
+  const digit2 = document.querySelector(`#digit2`)
+  const digit3 = document.querySelector(`#digit3`)
+  const digit4 = document.querySelector(`#digit4`)
+
+  let seconds1 = 0;
+  let seconds2 = 0;
+  let minutes1 = 0
+  let minutes2 = 0;
+
+  let interval;
+
+
+  // * Functionality - timer
+
+  function timer() {
+    seconds1++
+
+    if (seconds1 > 9) {
+      seconds1 = 0
+      seconds2++
+    }
+
+    if (seconds2 > 5) {
+      seconds2 = 0
+      minutes1++
+    }
+
+    if (minutes1 > 9) {
+      minutes1 = 0
+      minutes2++
+    }
+
+    if (minutes2 > 5) { minutes2 = 0 }
+
+    digit1.textContent = `${seconds1}`
+    digit2.textContent = `${seconds2}`
+    digit3.textContent = `${minutes1}`
+    digit4.textContent = `${minutes2}`
   }
-}
 
-//STOP TIME
 
-function stop() {
-  stopTime = Date.now();
-  if (started === true) {
-    started = false;
-    time = (stopTime - startTime) / 1000;
-    duration += time;
-    return stopTime;
-  } else {
-    return `Counting didn't start yet.`;
+  function start() {
+    if (interval) return
+    interval = setInterval(timer, 1000)
   }
+
+  function stop() {
+    clearInterval(interval)
+    interval = null
+  }
+
+  function reset() {
+    stop()
+    seconds1 = 0;
+    seconds2 = 0;
+    minutes1 = 0;
+    minutes2 = 0;
+
+    digit1.textContent = `${seconds1}`
+    digit2.textContent = `${seconds2}`
+    digit3.textContent = `${minutes1}`
+    digit4.textContent = `${minutes2}`
+  }
+
+  // * EventListeners - buttons
+
+  startButton.addEventListener('click', () => {
+    start()
+  })
+
+  stopButton.addEventListener('click', () => {
+    stop()
+  })
+
+  resetButton.addEventListener('click', () => {
+    reset()
+  })
+
 }
 
-//SHOW DURATION
-
-function durationTime() {
-  return duration;
-}
-
-//RESET TIME
-
-function reset() {
-  duration = 0;
-  startTime = null;
-  stopTime = null;
-  started = false;
-}
+stopwatch()
---
+++ steps.md
@@ -0,0 +1,9 @@
+Buttons :
+
+-start / stop button
+-reset button
+
+Screen :
+
+- active / stopped counting
+- result (displays over counting / smaller font)
